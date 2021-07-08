860a79d36cd8a1252d1046a22a528ed4ec5f2f1b
Hugo Perez <h.perez.saldivar@gmail.com>
2021-07-08 14:39:11 -0600

    Added fade in animation to index page

--- pages/index.js
+++ pages/index.js
@@ -15,16 +15,18 @@ export default function Home() {
 
       <main style={{maxWidth: "1200px"}} className="flex flex-col items-center w-full flex-1 py-20 w-full">
         <Nav />
-        <div style={{width: "100%", height: "500px"}} className="welcome-img mt-10 flex justify-center items-center">
-            <h1 className="text-white p-12 bg-black bg-opacity-60 text-3xl max-w-xl">Achieve a healthy, sustainable, sober life through mentoring, faith and community support.</h1>
-        </div>
-        <div className="flex justify-center mt-10">
-          <Link href="/application">
-            <a className={"text-lg py-2 px-5 border border-blue-600 text-blue-900 uppercase font-semibold tracking-wider mt-5" +
-            " cursor-pointer hover:bg-blue-900 hover:text-white hover:bg-opacity-90 " + transition_styles}>
-                Go to application
-            </a>
-          </Link>
+        <div className="fade-in w-full">
+          <div style={{width: "100%", height: "500px"}} className="welcome-img mt-10 flex justify-center items-center">
+              <h1 className="text-white p-12 bg-black bg-opacity-60 text-3xl max-w-xl">Achieve a healthy, sustainable, sober life through mentoring, faith and community support.</h1>
+          </div>
+          <div className="flex justify-center mt-10">
+            <Link href="/application">
+              <a className={"text-lg py-2 px-5 border border-blue-600 text-blue-900 uppercase font-semibold tracking-wider mt-5" +
+              " cursor-pointer hover:bg-blue-900 hover:text-white hover:bg-opacity-90 " + transition_styles}>
+                  Go to application
+              </a>
+            </Link>
+          </div>
         </div>
       </main>
 
